92f34ab104c5d3a5defba25c336cc68ab3ec711e
TomiT8 <tomas.jasko8@gmail.com>
2024-10-01 19:49:50 +0200

    pridanie

--- p02_sqlalchemy/connect_db.py
+++ p02_sqlalchemy/connect_db.py
@@ -5,7 +5,7 @@ from connection_details import *
 # db = create_engine('mysql+mysqlconnector://test:test@localhost:3306/cinematic')
 
 CONNECTION_SREING = "mysql+mysqlconnector://{user}:{password}@{host}:3306/{database}"
-db = create_engine(CONNECTION_SREING.format(user=user, password=password, host=host, database='cinematic'))
+db = create_engine(CONNECTION_SREING.format(user=user, password=password, host=host, database='school'))
 
 
 Session = sessionmaker(bind=db)
--- p02_sqlalchemy/p01_create_db.py
+++ p02_sqlalchemy/p01_create_db.py
@@ -1,5 +1,11 @@
 # pip install sqlalchemy
-
+from sqlalchemy_utils import database_exists, create_database
 from connect_db import *
+from models_student import Base
 
-print (f"db.url = {db.url}")
+print(f"db.url = {db.url}")
+
+if not database_exists(db.url):         # vytvorí
+    create_database(db.url)
+
+Base.metadata.create_all(db)            # vytvorí tabuľky z ried definovaných v models_students.py
