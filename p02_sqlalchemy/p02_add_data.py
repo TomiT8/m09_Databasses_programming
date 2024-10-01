78b5b8061e1eb05a0de931121a43229f5cc9f1df
TomiT8 <tomas.jasko8@gmail.com>
2024-10-01 20:44:02 +0200

    ukončený p02

--- p02_sqlalchemy/p02_add_data.py
+++ p02_sqlalchemy/p02_add_data.py
@@ -3,7 +3,17 @@ from models_student import Student
 
 student1 = Student(first_name='Adam', last_name='Bernau')
 session.add(student1)
+# session.add(Student(first_name="Adam", last_name="Bernau"))       # možný variant
+
+Students = [
+    Student(first_name='Adam', last_name='Slobodný'),
+    Student(first_name='Alica', last_name='Demečková'),
+    Student(first_name='Ciril', last_name='Demočko'),
+    Student(first_name='Matúš', last_name='Klaus'),
+    Student(first_name='Kamil', last_name='Bauer')
+]
+session.add_all(Students)
 
 session.commit()
 
-print("Študent bol pridaný.")
+print("Študenti boli pridaný.")
