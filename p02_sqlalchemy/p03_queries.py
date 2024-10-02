6f2eec0d7766e032d44b35107f4d287f4f660b54
TomiT8 <tomas.jasko8@gmail.com>
2024-10-02 18:39:45 +0200

    upravy

--- p02_sqlalchemy/p03_queries.py
+++ p02_sqlalchemy/p03_queries.py
@@ -1,3 +1,5 @@
+from sqlalchemy import and_, or_
+
 from connect_db import session
 from models_student import Student
 
@@ -12,3 +14,25 @@ for student in students:
 print("-"*40)
 total = session.query(Student).count()
 print(f"Počet študentov v databáze: {total}")
+
+print("-"*40)
+print("Študenti s id >=5:")
+"""SELECT * FROM students WHERE student.id >=5"""
+
+rows = session.query(Student).filter(Student.id >= 5)
+print(rows)
+for row in rows:
+    print(row)
+
+print("-"*40)
+print("Študenti s id > 3 a priezvyskom začínajúcim na 'Dem':")
+# rows = session.query(Student).filter(Student.id > 3, Student.last_name.like("Dem%"))
+# rows = session.query(Student).filter(and_(Student.id > 3, Student.last_name.like("Dem%")))
+for row in rows:
+    print(row)
+
+print("-"*40)
+print("Študenti, ktorých meno alebo priezvysko začína na 'A':")
+rows = session.query(Student).filter(or_(Student.first_name.like("A%"), Student.last_name.like("A%")))
+for row in rows:
+    print(row)
