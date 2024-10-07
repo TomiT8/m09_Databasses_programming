f821e3e200c601be170050ec8bf9be9b279215e3
TomiT8 <tomas.jasko8@gmail.com>
2024-10-07 19:15:46 +0200

    doplnený p02_insert_many

--- p04_mongo/p02_insert_many.py
+++ p04_mongo/p02_insert_many.py
@@ -7,11 +7,15 @@ customers = [
     #{"name": "Peter", "surname": "Sloboda", "address": "Jarná 1, Praha"},
     #{"name": "Ctibor", "surname": "Novotný", "address": "Letná 13, Olomouc"}
 
-    {"_id": 1, "name": "Dan", "surname": "Lejska", "address": "Zimná 12, Ostrava"},             # vytvoril som id
-    {"_id": 2, "name": "Evžen", "surname": "Brzobohatý", "address": "Košická 25, Ostrava"},
-    {"_id": 3, "name": "Filip", "surname": "Novák", "address": "Jesenského 15, Ostrava"},
-    {"_id": 4, "name": "Gustáv", "surname": "Adam", "address": "Janka Kráľa 5, Ostrava"},
-    {"_id": 5, "name": "Kamil", "surname": "Ludvík", "address": "Čemerná 22, Ostrava"}
+    #{"_id": 1, "name": "Dan", "surname": "Lejska", "address": "Zimná 12, Ostrava"},             # vytvoril som id
+    #{"_id": 2, "name": "Evžen", "surname": "Brzobohatý", "address": "Košická 25, Ostrava"},
+    #{"_id": 3, "name": "Filip", "surname": "Novák", "address": "Jesenského 15, Ostrava"},
+    #{"_id": 4, "name": "Gustáv", "surname": "Adam", "address": "Janka Kráľa 5, Ostrava"},
+    #{"_id": 5, "name": "Kamil", "surname": "Ludvík", "address": "Čemerná 22, Ostrava"}
+
+    {"_id": 6, "name": "Ivana"},
+    {"_id": 7, "name": "Jana", "surname": "Bledá", "email": "jana@bleda.sk"},
+    {"_id": 8, "name": "Karla", "surname": "Mráčiková", "phone_number": "+421999111333"}
 ]
 
 response = mycol.insert_many(customers)
