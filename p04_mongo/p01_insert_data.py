461d206523a7dd4bfaf70d0d24bb1ac5d21c447e
TomiT8 <tomas.jasko8@gmail.com>
2024-10-07 18:41:34 +0200

    doplnený p01_insert_data

---
+++ p04_mongo/p01_insert_data.py
@@ -0,0 +1,7 @@
+from connect_mango import mydb_pr24
+
+mycol = mydb_pr24["test_collection"]
+
+response = mycol.insert_one({"street_name": "jesenné", "number": 5, "city": "Košice"})
+
+print(response.inserted_id)
